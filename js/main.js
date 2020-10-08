d0a8d7f5285beaac25538f5c61ad4e51920d17e7
ashu <ashu61998@gmail.com>
2020-10-08 14:17:54 -0400

    adding even more sass

--- js/main.js
+++ js/main.js
@@ -1,6 +1,27 @@
+import team from './modules/DataModule.js';
+
 (() => 
 {
-console.log('loaded');
-console.log(Person);
+    let userSection = document.querySelector(".user-section"),
+    userTemplate = document.querySelector("#profs-template").content; 
 
-})();s
+    debugger
+    function handleDataSet(data){
+     
+
+        for(let user in data){
+     
+            let currentUser = userTemplate.cloneNode(true),
+            currentUserText = currentUser.querySelector(".user").children;
+     
+            currentUserText[1].textContent = data[user].name;
+            currentUserText[2].textContent = data[user].nickname;
+            currentUserText[3].textContent = data[user].role;
+
+            userSection.appendChild(currentUser);
+        }
+    }
+
+    handleDataSet(team);    
+
+})();
--- js/modules/DataModule.js
+++ js/modules/DataModule.js
@@ -1,1 +1,23 @@
-export 
+export default {
+  Ashwin: {
+      name: "Ashwin",
+      role: "Something",
+      nickname: "bruh"
+  },
+
+  John: {
+    name: "Ashwin",
+    role: "Something",
+    nickname: "bruh"
+},
+jared: {
+    name: "Ashwin",
+    role: "Something",
+    nickname: "bruh"
+},
+someone: {
+    name: "Ashwin",
+    role: "Something",
+    nickname: "bruh"
+}
+}
