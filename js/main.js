770cc7530e7f789115a2dbeaa904fb4ba127ffbe
ashu <ashu61998@gmail.com>
2020-10-08 14:51:37 -0400

    Ajax content

--- js/main.js
+++ js/main.js
@@ -2,10 +2,37 @@ import team from './modules/DataModule.js';
 
 (() => 
 {
+
+    let myReq = new XMLHttpRequest;
+    myReq.addEventListener("readystatechange", handleRequest);
+
+    myReq.open('GET', '../DataSet.json');
+    myReq.send();   
+    function handleRequest() {
+        if (myReq.readyState === XMLHttpRequest.DONE) {
+            // check status here and proceed
+            if (myReq.status === 200) {
+                // 200 means done and dusted, ready to go with the dataset!
+                debugger
+                handleDataSet(myReq.responseText);
+
+            } else {
+                // probably got some kind of error code, so handle that 
+                // a 404, 500 etc... can render appropriate error messages here
+                console.error(`${myReq.status} : something done broke, son`);
+            }
+        } else {
+            // request isn't ready yet, keep waiting...
+            console.log(`Request state: ${myReq.readyState}. Still processing...`);
+        }
+
+    }
+
+
     let userSection = document.querySelector(".user-section"),
     userTemplate = document.querySelector("#profs-template").content; 
 
-    debugger
+   
     function handleDataSet(data){
      
 
